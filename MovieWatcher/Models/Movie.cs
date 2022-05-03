b56cd1241833b3d42db0c98c2659cefb307b0bab
Vincent Beilke <vincent.beilke@gmail.com>
2022-05-03 10:17:28 -0500

    Added Models

---
+++ MovieWatcher/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWatcher.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int RatingId { get; set; }
+        public int TrailerId { get; set; }
+
+        public Rating Rating { get; set; }
+        public Trailer Trailer { get; set; }
+    }
+}
---
+++ MovieWatcher/Models/WatchList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWatcher.Models
+{
+    public class WatchList
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+
+        public User User { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
